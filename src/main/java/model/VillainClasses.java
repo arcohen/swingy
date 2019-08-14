ac85d16b21e41f68d48903f613f2b4650720d7cb
Ariel COHEN <arcohen@c2r3s4.wethinkcode.co.za>
2019-08-14 18:37:33 +0200

    model almost done

--- src/main/java/model/VillainClasses.java
+++ src/main/java/model/VillainClasses.java
@@ -5,6 +5,7 @@
  */
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,4 +14,25 @@ import java.util.List;
  */
 public class VillainClasses {
     List<VillainClass> villainClasses;
+    
+        String filePath = "../../../CharacterInfo/villain_classes.txt";
+        
+    try 
+    {
+        ReadFile file = new ReadFile(filePath);
+        ArrayList<String> textLines = file.OpenFile();
+        
+        textLines.remove(0);
+        
+        for (String line : textLines) {
+            String[] villainInfo;
+            villainInfo = line.split(";");
+            VillainClass villain = new VillainClass(villainInfo);
+            villainClasses.add(villain);
+        }
+    } 
+    catch (IOExeption e) 
+    {
+        System.out.println(e.Message());
+    }
 }
