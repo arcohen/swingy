d9a8ee7952612593724cf5b626b8119e243074e6
Ariel COHEN <arcohen@c3r2s4.wethinkcode.co.za>
2019-09-13 17:25:01 +0200

    sql

--- src/main/java/utilities/DBLookup.java
+++ src/main/java/utilities/DBLookup.java
@@ -67,5 +67,61 @@ public class DBLookup {
         }
     }
 
-    public void saveHero(int id, String name, int attack, int defense, int HitPoints) 
+    public void updateHero(int id, String name, int attack, int defense, int hitPoints, int level, int xp) {
+        
+        try {
+            Connection conn = connect();
+
+            String sql = 
+                "UPDATE HERO " +
+                "SET (?, ?, ?, ?, ?, ?) " +
+                "WHERE Id = ?";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(sql);
+
+            preparedStmt.setString(1, name);
+            preparedStmt.setInt(2, attack);
+            preparedStmt.setInt(3, defense);
+            preparedStmt.setInt(4, hitPoints);
+            preparedStmt.setInt(5, level);
+            preparedStmt.setInt(6, xp);
+            preparedStmt.setInt(7, id);
+
+            preparedStmt.executeUpdate(sql);
+
+            preparedStmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void saveHero(String name, int attack, int defense, int hitPoints) {
+         
+        try {
+            Connection conn = connect();
+
+            String sql = 
+                "INSERT INTO HERO " +
+                "VALUES (?, ?, ?, ?, ?, ?) ";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(sql);
+
+            preparedStmt.setString(1, name);
+            preparedStmt.setInt(2, attack);
+            preparedStmt.setInt(3, defense);
+            preparedStmt.setInt(4, hitPoints);
+            preparedStmt.setInt(5, 1);
+            preparedStmt.setInt(6, 1000);
+
+            preparedStmt.executeUpdate(sql);
+
+            preparedStmt.close();
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
